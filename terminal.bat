b17e2bbfd8aab1935e1bcc1ea8cd25f26ee3fb2d
Zawadzki <134331872+tinyengine1@users.noreply.github.com>
2024-03-26 22:33:55 +0100

    Add files via upload

---
+++ terminal.bat
@@ -0,0 +1,25 @@
+@echo off
+
+title Terminal
+
+chcp 65001 >nul
+
+echo [36m**********************************************************
+echo ** Zawadzki's Terminal v1                               **
+echo ** For private use only. Redistribution is prohibited.  **
+echo **********************************************************[0m
+
+for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
+
+:input
+echo.
+
+echo [95m╔══[0m([33m%username%[0m@[92m%computername%[0m)-[[94m%cd%[0m]
+set /p cmd=".%BS%[95m╚══>[0m "
+
+echo.
+
+%cmd%
+echo Finished with code %errorlevel%
+
+goto input
